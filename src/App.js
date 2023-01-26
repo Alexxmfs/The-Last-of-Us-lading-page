2069f503007031b75348aa493badddb533e6ca0d
Alexxmfs <alexxmfs@hotmail.com>
2023-01-26 16:32:32 -0300

    buidling style navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "./Components/Navbar";
+import "./Styles/main.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
